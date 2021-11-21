1fde8aed308e39ce8c13c964dce7a3cb9c25d10e
jimathy <gothicjim@gmail.com>
2021-11-21 02:33:15 +0000

    Delete qb-pokemonv2 directory

--- qb-pokemonv2/gl_server.lua
+++
@@ -1,9 +0,0 @@
-QBCore = nil
-
-TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-
---[[RegisterNetEvent('gl-cards:showPlayer')
-AddEventHandler('gl-cards:showPlayer',function(ID, targetID, card)
-	local _source = QBCore.Functions.GetPlayer(source)
-	TriggerClientEvent('gl-cards:drawNui', _source, card)
-end)]]
